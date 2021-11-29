bbab92e3b9bd2903bd9ef876ede7a9c535b9b3d0
aldl1910 <antonioluisdl@gmail.com>
2021-11-29 21:55:22 +0100

    contents all

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
